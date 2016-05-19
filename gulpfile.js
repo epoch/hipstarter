579e2cada1890a75a94ebf86a1e03e637a257182
Daniel Tsui <tsui.daniel@gmail.com>
2016-05-20 00:26:22 +1000

    fix sass task should generate bundle with css extension. add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 /*eslint-env node*/
 var gulp = require('gulp');
+var runSync = require('run-sequence');
 var plugin = {};
 
+plugin.server = require('browser-sync').create();
 plugin.rename = require('gulp-rename');
 plugin.sass = require('gulp-sass');
 plugin.autoprefixer = require('gulp-autoprefixer');
@@ -9,17 +11,35 @@ plugin.eslint = require('gulp-eslint');
 
 gulp.task('sass', sass);
 gulp.task('lint', lint);
-gulp.task('watch', function() {
-  gulp.watch('./scss/**/*.scss', ['sass']);
-});
-gulp.task('default', ['watch']);
+gulp.task('serve', serve);
+gulp.task('refresh', refresh);
+gulp.task('watch', watch);
+gulp.task('default', ['serve', 'watch']);
+
+function refresh() {
+  plugin.server.stream();
+}
+
+function watch() {
+  gulp.watch('./scss/**/*.scss', function() {
+    runSync('sass', 'refresh');
+  });
+  gulp.watch('./index.html').on('change', plugin.server.reload);
+}
+
+function serve() {
+  plugin.server.init({
+    server: './'
+  });
+}
 
 function sass() {
   gulp.src('./scss/main.scss')
   .pipe( plugin.sass() )
   .pipe( plugin.autoprefixer() )
-  .pipe( plugin.rename('bundle.js') )
-  .pipe( gulp.dest('bundle') );
+  .pipe( plugin.rename('bundle.css') )
+  .pipe( gulp.dest('bundle') )
+  .pipe( plugin.server.stream() );
 }
 
 function lint() {
